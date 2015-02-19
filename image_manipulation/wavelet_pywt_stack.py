4e1df460b085b7c9cb8582380b6d839517dbf17f
tpnspign@gmail.com <tpnspign@gmail.com@520afdbc-3f0f-8298-e3f3-48e2bb560311>
2015-02-19 08:37:09 +0000

    Added folder for image manipulation and uploaded my wavelet code

---
+++ image_manipulation/wavelet_pywt_stack.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 24 14:46:47 2014
+
+@author: ralph
+"""
+
+import Image
+import os
+import numpy as np
+import pywt
+import matplotlib.pyplot as plt
+
+mode = 'haar'   # Defining the used wavelet
+level = 2       # Defining iteration level  
+
+path = '/path/to/input/stack/'
+dir_out = '/path/to/output/stack/'
+
+files = os.listdir(path)
+files = sorted(files)
+
+im = Image.open(path + files[1]).convert('L')
+print files[1]
+
+for i in range(len(files)):
+    name = files[i]
+    name_comp = path + name
+    print name
+    
+    im = Image.open(name_comp)
+
+
+    # compute coefficients of multiresolution WT
+    coeffs=pywt.wavedec2(im, mode,level=level)
+
+
+    # high frequency coeffs
+    coeffs_H=list(coeffs)
+
+    # discarding the low frequency
+    # Approximation coeffs are from the low-pass filter
+    coeffs_H[0]=np.zeros(coeffs_H[0].shape)
+    # multilevel reconstruction
+    imArray_H=pywt.waverec2(coeffs_H, mode)
+
+    im_wv = Image.fromarray(imArray_H) 
+    im_wv.save(dir_out + 'filename_' + str(i) + '.tif')
+
+
+
+
